133d83e18c80f06118f9e470ad307ebe5de87c1f
ShadowLemoon <119576779+ShadowLemoon@users.noreply.github.com>
2026-08-20 10:18:35 +0800

    feat: 初始化 WinUI 主窗口行为

--- src/Sparxie.Desktop/MainWindow.xaml.cs
+++ src/Sparxie.Desktop/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
+using WinRT.Interop;
 
 namespace Sparxie.Desktop;
 
@@ -8,5 +10,26 @@ public sealed partial class MainWindow : Window
     {
         InitializeComponent();
         Title = "Sparxie";
+
+        TryConfigureWindow();
+    }
+
+    private void TryConfigureWindow()
+    {
+        var hwnd = WindowNative.GetWindowHandle(this);
+        var windowId = Win32Interop.GetWindowIdFromWindow(hwnd);
+        var appWindow = AppWindow.GetFromWindowId(windowId);
+
+        if (appWindow.Presenter is OverlappedPresenter presenter)
+        {
+            presenter.IsResizable = true;
+            presenter.IsMaximizable = true;
+        }
+
+        appWindow.Resize(new Windows.Graphics.SizeInt32
+        {
+            Width = 1280,
+            Height = 800
+        });
     }
 }
